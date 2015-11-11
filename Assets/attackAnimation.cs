7f0eec7dba600dcbfb7258bdeb2229172e9e13f8
jbouse <jbouse@emich.edu>
2015-11-11 10:39:32 -0500

    actual added animations

---
+++ Assets/attackAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attackAnimation : MonoBehaviour {
+
+	public void attack(){
+		GetComponent<Animation> ().Play ("macheteSwing");
+		if(!GetComponent<Animation>().IsPlaying("macheteSwing")){
+			GetComponent<Animation>().Play("macheteIdle");
+		}
+		//GetComponent<Animation>()["macheteSwing"].speed = -1;
+		//GetComponent<Animation> () ["macheteSwing"].time = 0;
+		//GetComponent<Animation> ().Play ("macheteIdle");
+	}
+
+	public void reset(){
+		if(!GetComponent<Animation>().IsPlaying("macheteSwing")){
+			GetComponent<Animation>().Play("macheteIdle");
+		}
+	}
+
+}
