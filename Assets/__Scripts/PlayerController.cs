654d87f7d0dafa39296652751761476eae9b39d1
amandamc72 <amccart8@emich.edu>
2015-11-22 23:26:30 -0500

    Player Health and gui
    - added 2d mat to lvl 1
    - health and treasure ui text
    - player damage from hazards
    - player respawn

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System;
 using System.Threading;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class PlayerController : MonoBehaviour
     public float runSpeed = 2f;
     public float jumpForce = 200f;
     public bool isGrounded;
-    public int playerHealth = 100;
+    public int playerHealth;
+	public Text healthText;
     public int attackDamage;
     public Transform groundCheck;
     public LayerMask whatIsGround;
     public float groundRadius = 0.2f;
+	public GameObject spawnPoint;
 
     /* **** PRIVATE **** */
     private Rigidbody2D player;
@@ -31,6 +34,8 @@ public class PlayerController : MonoBehaviour
         jumpedOnce = false;
         jumpedTwice = false;
         facingRight = true;
+		playerHealth = 100;
+		SetHealthText();
     }
 
     /* **** EXECUTED ONCE PER PHYSICS STEP **** */
@@ -103,11 +108,40 @@ public class PlayerController : MonoBehaviour
         player.velocity = new Vector2(movement * walkSpeed * runSpeed, player.velocity.y);
         animator.SetFloat("Walk", Mathf.Abs(movement));
     }
+	
+	//Damage from hazards
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Hazard")
+        {
+            playerHealth -= 5;
+            SetHealthText();
+        }
+    }
+
+    //Set playerHealth ui text
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + playerHealth;
+    }
 
 
-    void Death() { }
+    //Kill the player
+    void Death()
+    {
+        if (playerHealth <= 0)
+        {
+            Respawn();
+        }
+    }
 
-    void Respawn() { }
+    //Respawn the player
+    void Respawn()
+    {
+        this.transform.position = spawnPoint.transform.position;
+        playerHealth = 100;
+        SetHealthText();
+    }
 
     /* Flip ensures smooth animations for the object. Rather than run an animation
      * for each direction the object faces, it simply turns the animation the opposite
