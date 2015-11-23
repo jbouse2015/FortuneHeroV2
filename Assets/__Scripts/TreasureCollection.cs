33da79210c0d0db6d11bba8be13433bb6ede3f43
amandamc72 <amccart8@emich.edu>
2015-11-22 22:40:22 -0500

    PLayer health and respawn
    - added lvl 1 2d mat
    - player damage from hazards
    - player respawn
    - gui for counts

---
+++ Assets/__Scripts/TreasureCollection.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TreasureCollection : MonoBehaviour {
+
+    private int treasureTotal;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        treasureTotal = 7;
+        SetCountText();
+        winText.text = "";
+    }
+
+    //if we collide with treasure destroy and decrement total
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Treasure") {
+            treasureTotal -= 1;
+            Destroy(collision.gameObject);
+            SetCountText(); 
+        }
+    }
+
+    //Set treasureTotal ui text
+    void SetCountText()
+    {
+        countText.text = "Treasure Remaining: " + treasureTotal.ToString();
+        if (treasureTotal == 0)
+        {
+            winText.text = "Level Complete!";
+        }
+    }
+}
+
+
