a324722c17953f91cc45b5088b19f4363bbc6c51
Brandon Bush <brandontbush@gmail.com>
2015-11-25 12:28:39 -0500

    Added simple controller for Bat to follow the player.

---
+++ Assets/__Scripts/BatController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatController : MonoBehaviour {
+
+    public Transform target;//set target from inspector instead of looking in Update
+    public float speed = 1.5f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);//correcting the original rotation
+        if (Vector3.Distance(transform.position, target.position) > 1f)
+        {//move if distance from target is greater than 1
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        
+    }
+}
